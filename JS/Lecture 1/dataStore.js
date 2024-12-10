83c63738ee9eea537b059fb5ce36f9c130c2231a
chhavi2005 <chhavigajnani5@gmail.com>
2024-12-10 10:12:10 +0530

    Create JS/Lecture 1/dataStore.js

---
+++ JS/Lecture 1/dataStore.js
@@ -0,0 +1,42 @@
+// PRINT IN JS
+    console.log("Hello Coder Army !!!")
+
+// HISTORY OF JS
+    // google chrome has v8 engine - js compiler basically for chrome but not exactly compiler it JIT - Just In Time
+    // to make js run fast
+    // spidermonkey is the engine of js for mozilla firefox
+    // browser make their js engine but have to follow standards by ECMA - europe organization which decide standards for programming and other language stuff
+    // js engine - js code into binary (js engine is also a code which converts)
+    // compiler work - convert code into binary
+    // v8 engine - famous because its fast
+    // js engine is made through c++(70%+) because it is fast
+    // in browser there is engine already injected to run js but on vs code any js engine is not there so node.js was made
+    // to run js outside the browser
+    // node.js - v8engine + extra functionality
+    // install node.js
+    // by writing node file_name in terminal we can use it in vs code
+    // node.js is used in frontend (earlier) and now also backend
+
+// STORE DATA IN JS
+    // let const var
+
+    // let
+    // no need to define datatype just make a variable with let at starting
+    let num = 10;
+    console.log(num);
+
+    let naam = "Chhavi";
+    naam = "Vanya"
+    console.log(naam);
+
+    // const
+    // ṭo make a constant variable which can't be changed if re assign
+    const pin = 10023
+    console.log(pin)
+    // pin=33109
+    // we get an error
+
+    // var
+    // same as let but old way now let is used instead
+    // some disadvantage related to block scope 
+    // DON'T USE IT !!
