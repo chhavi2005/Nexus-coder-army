c3a606e61d82489fadcdd67f5f76aaa2fa5c9413
chhavi2005 <chhavigajnani5@gmail.com>
2024-12-12 10:12:10 +0530

    Create JS/Lecture 3/operator.js

---
+++ JS/Lecture 3/operators.js
@@ -0,0 +1,17 @@
+// OPERATORS - arithmetic,assignment,comparison,logical,bitwise
+
+console.log(((6*(3+18)/6)-9))
+// prescedence 
+// bracket
+// divide multiply - left to right
+// add sub - left to right
+
+console.log(20%3)
+// gives remainder
+
+// var++ post increment, var-- post decrement
+// ++var pre increment, --var pre decrement
+let su = 20
+console.log(su++)
+let ju = 20
+console.log(++ju)
